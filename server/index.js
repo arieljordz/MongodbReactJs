e4727d4eff8b4d20e542c0efea214e3cfe44b249
arieljordz <arieljordz@gmail.com>
2025-03-24 00:38:55 +0800

    update frontend url

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 // ✅ Better CORS Handling
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://mongodb-react-js.vercel.app"],
+  origin: ["http://localhost:5173", "https://e-learning-dun-phi.vercel.app"],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type",
 };
